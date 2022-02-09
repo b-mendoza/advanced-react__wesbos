5041c1b4044664f42fd3717077089dadbb5b6a7a
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-09 17:54:25 -0600

    feat: update test from `IndexPage` component

--- frontend/__tests__/pages/index.test.tsx
+++ frontend/__tests__/pages/index.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
 
-import Home from '@/pages/index';
+import IndexPage from '@/pages/index';
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />);
+describe('Tests to IndexPage component', () => {
+  it('Takes a Snapshot of the `IndexPage` component', () => {
+    const { container } = render(<IndexPage />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Renders initial JSX from `IndexPage` component', () => {
+    render(<IndexPage />);
 
     const heading = screen.getByRole('heading', { name: /hello world/i });
 
