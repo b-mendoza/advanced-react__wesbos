dfe197529c2be7eb895197bcd4937d99f0c6bdf5
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-09 01:53:31 -0600

    feat: add Jest `config` file

---
+++ frontend/jest.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from '@jest/types';
+
+const jestConfig: Config.InitialOptions = {
+  collectCoverageFrom: [
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '**/*.{js,jsx,ts,tsx}',
+  ],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    // Handle Image imports
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': `<rootDir>/__mocks__/fileMock.js`,
+
+    // Handle Module Aliases
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+};
+
+export default jestConfig;
