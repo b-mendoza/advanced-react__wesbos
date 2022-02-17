fef70ddfd3fecb376c45a1b6c5c68a2e92d550f2
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-16 23:45:23 -0600

    feat: add `Nav` component

---
+++ frontend/components/Nav/index.tsx
@@ -0,0 +1,31 @@
+import { memo } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { LinkTo } from '@/components/LinkTo';
+
+interface Route {
+  id: string;
+  path: string;
+  text: string;
+}
+
+const routeList: Route[] = [
+  { id: uuidv4(), path: '/products', text: 'Products' },
+  { id: uuidv4(), path: '/sell', text: 'Sell' },
+  { id: uuidv4(), path: '/orders', text: 'Orders' },
+  { id: uuidv4(), path: '/account', text: 'Account' },
+];
+
+export function Nav(): JSX.Element {
+  return (
+    <nav>
+      <ul>
+        {routeList.map((route) => (
+          <li key={route.id}>
+            <LinkTo href={route.path}>{route.text}</LinkTo>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
